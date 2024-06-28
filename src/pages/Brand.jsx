59e8357e5e188021dd2175b568da5b68a8de3f95
Jojjosthlm <joanna.olofsson@chasacademy.se>
2024-06-28 17:00:21 +0200

    fixed image paths

--- src/pages/Brand.jsx
+++ src/pages/Brand.jsx
@@ -2,9 +2,9 @@ import styled from "styled-components";
 import { Container, StyledHeader, StyledHeadline  } from "../styles/Containers";
 import { StyledP, StyledH4, StyledH2, StyledH5 } from "../styles/Fonts.styled";
 import Back from "../components/Back/GoBack";
-import brandPrincipImg from '../assets/brand_principer.png';
+import brandPrincipImg from '../assets/Brand_principer.png';
 import brandTargetImg from '../assets/Brand_target.png';
-import brandCompImg from '../assets/brand_components.png';
+import brandCompImg from '../assets/Brand_components.png';
 
 function Brand() {
     return(
