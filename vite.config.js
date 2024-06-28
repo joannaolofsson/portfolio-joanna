15e4ec9219ac1bac51f2eef4be691a258b60492a
Jojjosthlm <joanna.olofsson@chasacademy.se>
2024-06-28 16:38:08 +0200

    another 240628

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
-})
+export default defineConfig(() => {
+  return {
+    define: {
+      __APP_ENV__: process.env.VITE_VERCEL_ENV,
+    },
+  };
+});
